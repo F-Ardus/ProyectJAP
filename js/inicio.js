32d90a97af22f19a8773370a6745090722e20c70
Fabio Ardus <kigoromous@gmail.com>
2017-12-02 13:09:33 -0300

    Puliendo1

--- js/inicio.js
+++ js/inicio.js
@@ -16,6 +16,11 @@ $( document ).ready(function() {
 			$(this).parent().addClass("hidenInfo");
 		});
 
+		$('#exampleModal').on('show.bs.modal', function (e) {
+	    var image = $(e.relatedTarget).attr('src');
+	    $(".modalImg").attr("src", image);
+	  });
+
 		$('#myModal').on('shown.bs.modal', function () {
 	  	$('#myInput').trigger('focus')
 		})
